a694b8d8b448dbc4fb32c320c86136398efeb921
Alma Perez <aperez32@student.scf.edu>
2022-07-30 15:49:56 -0400

    First Class Function 1

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callback){
+callback();
+}
+function returnsANamedFunction(){
+    return returnsANamedFunction
+}
+const returnsAnAnonymousFunction = function () {
+return returnsAnAnonymousFunction();
+}
